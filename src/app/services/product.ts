c935d50bf3bc6b0b078b8759ad4dfedc3b45f7f4
Lara Newsom <laramnewsom@gmail.com>
2025-07-14 23:02:06 -0700

    initial(): Set up code demo for 05_03 (#31)
    * updated gitignore and vscode settings
    
    * initial commit
    
    * fixed gitignore issues
    
    * fixed styling
    
    * deleted unused test file
    
    * implemented OnPush
    
    * updated proerties to use signals
    
    * return white space
    
    * added an effect
    
    * updated code to include dynamic shipping options
    
    * implement linked signal
    
    * added shipping service to fetch shipping options
    
    * added timezone selection
    
    * added toSignal
    
    * set up http resource demo
    
    * added angular-in-memory-web-api
    
    * updated shipping service to use http resource
    
    * added side nav and detail view
    
    * implemented rxResource in detail view
    
    * setting up input demo
    
    * updated to use input function
    
    * cleaned up unused imports
    
    * set up two way binding
    
    * implemented model input function
    
    * added all products view
    
    * fixed lint issues
    
    * implemented signal query functions
    
    * added coPilot instructions
    
    * added eslint
    
    * ran lint with fix flag
    
    * used coPilot to update my components
    
    * updated package lock
    
    * reverting back to decorator based
    
    * ran the input migration schematic
    
    * updated input
    
    * ran query schematic
    
    * set up for demo

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,14 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Item } from './product-data';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Product {
   private readonly httpClient = inject(HttpClient);
-  private readonly router = inject(Router);
 
   getProducts() {
     return this.httpClient.get<Item[]>(`api/items`)
