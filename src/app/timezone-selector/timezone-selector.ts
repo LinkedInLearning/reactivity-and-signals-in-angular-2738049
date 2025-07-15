e079ea547775a765c2fc7a0690d02a97b27db421
Lara Newsom <laramnewsom@gmail.com>
2025-07-14 22:52:36 -0700

    final(): Completed code changes for 05_01 (#28)
    * updated gitignore and vscode settings
    
    * initial commit
    
    * fixed gitignore issues
    
    * fixed styling
    
    * deleted unused test file
    
    * implemented OnPush
    
    * updated proerties to use signals
    
    * return white space
    
    * added an effect
    
    * updated code to include dynamic shipping options
    
    * implement linked signal
    
    * added shipping service to fetch shipping options
    
    * added timezone selection
    
    * added toSignal
    
    * set up http resource demo
    
    * added angular-in-memory-web-api
    
    * updated shipping service to use http resource
    
    * added side nav and detail view
    
    * implemented rxResource in detail view
    
    * setting up input demo
    
    * updated to use input function
    
    * cleaned up unused imports
    
    * set up two way binding
    
    * implemented model input function
    
    * added all products view
    
    * fixed lint issues
    
    * implemented signal query functions
    
    * added coPilot instructions
    
    * added eslint
    
    * ran lint with fix flag
    
    * used coPilot to update my components
    
    * updated package lock

--- src/app/timezone-selector/timezone-selector.ts
+++ src/app/timezone-selector/timezone-selector.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ShippingService } from '../services/shipping';
 
 @Component({
   selector: 'app-timezone-selector',
   imports: [],
   templateUrl: './timezone-selector.html',
-  styleUrl: './timezone-selector.scss'
+  styleUrl: './timezone-selector.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimezoneSelector {
   protected readonly shippingService = inject(ShippingService);
